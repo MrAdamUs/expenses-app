9d29410ca48c60bfb72d7c3d1a657bffdac8368a
Adam Mawlawi <adam@mradam.us>
2021-03-17 16:17:28 -0500

    create dropdown sorting filter

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 10000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 106500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
