f06f6a1e14454289beb32b868e73936fed076921
Adam Mawlawi <adam@mradam.us>
2021-03-23 13:37:50 -0500

    add firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/database';
 var firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  databaseURL: 'https://expensify-1e235-default-rtdb.firebaseio.com',
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
